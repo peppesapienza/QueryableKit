87171847e178ded4b14c50515ae1e9ba7e3c14ea
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2023-03-27 18:09:55 +1100

    Convert value to `rawValue` if `RawRepresentable`

--- Sources/Firestore/PredicateComposer.swift
+++ Sources/Firestore/PredicateComposer.swift
@@ -5,24 +5,30 @@ import FirebaseFirestoreSwift
 struct FirestorePredicateComposer: PredicateVisitor {
     
     func visit<Path, PathType, Value>(_ predicate: Field<Path, PathType, Value>, in context: inout Query) throws {
+        var value: Any = predicate.value
+        
+        if let raw = predicate.value as? any RawRepresentable {
+            value = raw.rawValue
+        }
+        
         switch predicate.operator {
         case .isEqualTo:
-            context = context.whereField(try predicate.field(), isEqualTo: predicate.value)
+            context = context.whereField(try predicate.field(), isEqualTo: value)
             
         case .isGreaterThan:
-            context = context.whereField(try predicate.field(), isGreaterThan: predicate.value)
+            context = context.whereField(try predicate.field(), isGreaterThan: value)
             
         case .isLessThan:
-            context = context.whereField(try predicate.field(), isLessThan: predicate.value)
+            context = context.whereField(try predicate.field(), isLessThan: value)
             
         case .isGreaterThanOrEqualTo:
-            context = context.whereField(try predicate.field(), isGreaterThanOrEqualTo: predicate.value)
+            context = context.whereField(try predicate.field(), isGreaterThanOrEqualTo: value)
             
         case .isLessThanOrEqualTo:
-            context = context.whereField(try predicate.field(), isLessThanOrEqualTo: predicate.value)
+            context = context.whereField(try predicate.field(), isLessThanOrEqualTo: value)
             
         case .contains:
-            context = context.whereField(try predicate.field(), arrayContains: predicate.value)
+            context = context.whereField(try predicate.field(), arrayContains: value)
             
         case .isAnyOf:
             context = context.whereField(try predicate.field(), arrayContainsAny: predicate.value as! [Any])
