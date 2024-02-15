b701a48bf21a221c8923d19a53969471ae75de38
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-02-15 20:16:16 +1100

    Removes unnecessary `import QueryableMacros`

--- Sources/Core/Queryable+Macro.swift
+++ Sources/Core/Queryable+Macro.swift
@@ -1,5 +1,3 @@
-import QueryableMacros
-
 /// The `Queryable` macro defines a type that can be queried
 /// using predicates conforming to the `QueryablePredicate` protocol.
 @attached(member, names: named(field))
